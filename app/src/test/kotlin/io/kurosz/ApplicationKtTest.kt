c1ac5d6f1fc5a1541bde88d72c0c50c547b0da95
Daniel Kurosz <danielkurosz@gmail.com>
2023-09-13 23:02:47 +0200

    add tests

---
+++ app/src/test/kotlin/io/kurosz/ApplicationKtTest.kt
@@ -0,0 +1,58 @@
+package io.kurosz
+
+import io.kurosz.driver.EmployeeDto
+import io.kurosz.driver.GetHiredUsers
+import io.kurosz.driver.HireEmployeeUseCase
+import io.kurosz.employee.UserController
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@ExtendWith(SpringExtension::class)
+@WebMvcTest(controllers = [UserController::class])
+class ApplicationKtTest(@Autowired val mockMvc: MockMvc) {
+
+
+    @TestConfiguration
+    class ControllerTestConfig {
+        @Bean
+        fun hireEmployeeUseCase() = mockk<HireEmployeeUseCase>()
+
+        @Bean
+        fun getHiredUsers() = mockk<GetHiredUsers>()
+    }
+
+    @Autowired
+    private lateinit var getHiredUsers: GetHiredUsers
+
+    @Test
+    fun shouldReturnListOfEmployees() {
+        every { getHiredUsers.getAll() } returns listOf(EmployeeDto("1", "", "", 22))
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value("1"));
+    }
+
+    @Test
+    fun shouldReturnEmptyListOfEmployees() {
+        every { getHiredUsers.getAll() } returns listOf()
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.content().string("[]"))
+    }
+
+}
